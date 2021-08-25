b5f0afb40f4eec6da3a8a6e54f0fd736470ddffa
Avikal Shukla <shuklaavikal7@gmail.com>
2021-08-25 11:39:19 +0530

    Fixed Some Bugs And Issues

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,11 +2,26 @@ var friends = JSON.parse(localStorage.getItem("friends"))
 
 var list = ''
 console.log(list)
-// list = `<h2 id="friends">Friends</h2>`
+
 for (i=0; i< friends.length;  i++)
 {
     console.log(friends[i])
     list += `<div class="card" id="${i}"><h4>${friends[i]}</h4></div>`
-}   
+}
+
+function openLink(e, info) {
+    var i, content, links;
+    content = document.getElementsByClassName("fadeIn");
+    for (i = 0; i < content.length; i++) {
+    content[i].style.display = "none";
+    }
+    links = document.getElementsByClassName("demo");
+    for (i = 0; i < links.length; i++) {
+    links[i].className = links[i].className.replace(" active", "");
+    }
+    document.getElementById(info).style.display = "block";
+    e.currentTarget.className += " active";
+    }
+    document.getElementById("current").click();
 
 document.getElementById('friends').innerHTML = list
