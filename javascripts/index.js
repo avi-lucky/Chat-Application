366bd4aa8a69960902908bd8cee6b6ae578d9617
Avikal Shukla <shuklaavikal7@gmail.com>
2021-09-01 12:07:13 +0530

    Chat View Under Maintenance

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,7 +3,7 @@ var friends = JSON.parse(localStorage.getItem("friends"))
 var list = ''
 console.log(list)
 
-for (i=0; i< friends.length;  i++)
+for (i=0; i< friends.length; i++)
 {
     console.log(friends[i])
     list += `<button class="card" id="${i}" onclick="openClick(event)"><h4>${friends[i]}</h4></button><br>`
@@ -26,46 +26,30 @@ for (i=0; i< friends.length;  i++)
     // document.getElementById("current").click();
 
 function openClick(e) {
-    var i, cards, friends;
-    console.log(i)
-    friends = document.getElementById("friends");
+    var i, cards;
     cards = document.getElementsByClassName("card");
     console.log(document.getElementsByClassName("card"))
     for (i = 0; i < cards.length; i++) {
-        // if(!e.currentTarget.className.includes("active")) {
-        //     e.currentTarget.className += " active";
-        // }
-            // var current = document.getElementsByClassName("active");
-            cards[0].className = cards[0].className.replace(" active", "");
-            cards[1].className = cards[1].className.replace(" active", "");
-            e.currentTarget.className += " active";
-    }
-    // document.getElementById(e).style.display = "block";
-        // var i, cards;
-        // cards = document.getElementsByClassName("card");
-        // for (i = 0; i < cards.length; i++) {
-        // //     if(!e.currentTarget.className.includes("active")){
-        // //     e.currentTarget.className += " active";
-        // // }
-        //   cards[i].className = cards[i].className.replace(" active ", "");
-        //   console.log(i) 
-        // }
-        // e.currentTarget.className += " active";
-        // document.getElementById(e).style.display = "block";
-}
-      
-    // document.getElementById('friends').style.display = "block";
-    // if(!e.currentTarget.className.includes("active")){
-    //     e.currentTarget.className += " active";
-    // }
-    // console.log(e.currentTarget.className)
-    // console.log(e)
-    // document.getElementById('friends').click();
-
+            cards[i].className = cards[i].className.replace(" active", "");
+        }
+        e.currentTarget.className += " active";
+}      
 document.getElementById('friends').innerHTML = list
 
 var chat = JSON.parse(localStorage.getItem('chat'))
+var list = ''
+console.log(list)
+
+for (i=0; i< chat.length; i++)
+{
+    console.log(chat[i])
+    list += `<div class="chats" id="${i}"><h2>${chat[i]}</h2></div>`
+}
 
 var chat = [["Himanshu","Hi",1212443],["Avikal", "Hello", 1223435],["Himanshu", "How are you", 1324355],["Avikal", "I am good.", 1243545]]
 
+friends.push(document.getElementById("chat").value)
+
 localStorage.setItem("chat", JSON.stringify(chat))
+
+document.getElementById('chat').innerHTML = list
