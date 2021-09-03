42325f986ead44a72aaa1f05592a848db2ba3a7c
Avikal Shukla <shuklaavikal7@gmail.com>
2021-09-03 16:50:41 +0530

    Chat View Under Maintenance

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,6 +3,9 @@ var friends = JSON.parse(localStorage.getItem("friends"))
 var list = ''
 console.log(list)
 
+var AayushPandey = [["Aayush Pandey","Hii",1212443],["Avikal Shukla", "Hello", 1223435],["Aayush Pandey", "How are you", 1324355],["Avikal Shukla", "I am good.", 1243545]]
+var AryanGupta = [["Aryan Gupta","Hey",1212443],["Avikal Shukla", "Hii", 1223435],["Aryan Gupta", "How are you", 1324355],["Avikal Shukla", "I am fine.", 1243545]]
+
 for (i=0; i< friends.length; i++)
 {
     console.log(friends[i])
@@ -29,44 +32,39 @@ document.getElementById('friends').innerHTML = list
 //     }
 //     document.getElementById("current").click();
 
+chat(window[friends[0].split(" ").join("")])
+
 function openClick(e) {
-    // var i, table, cards;
-    // table = document.getElementsByClassName("chatTable");
-    // console.log(e.currentTarget.getAttribute('value'))
-    // for (i = 0; i < table.length; i++) {
-    //     table[i].style.display = "none";
-    // }
     var cards = document.getElementsByClassName("card");
-    // console.log(document.getElementsByClassName("card"))
     for (i = 0; i < cards.length; i++) {
             cards[i].className = cards[i].className.replace(" active", "");
         }
         // document.getElementById(user).style.display = "block";
     e.currentTarget.className += " active";
-        // console.log(e)
     document.getElementById('chatTitle').innerHTML = e.currentTarget.getAttribute('value')
+    console.log(e.currentTarget.getAttribute('value').split(" ").join(""))
+    chat(window[e.currentTarget.getAttribute('value').split(" ").join("")])
 
 }
 // document.getElementById("friends").click();      
 
-// var aayush = JSON.parse(localStorage.getItem('Aayush Pandey'))
+function chat(name){
+    var list = ''
+    console.log(list)
+
+    for (i=0; i < name.length; i++)
+    {
+        console.log(name[i])
+        list += `<div class="chats" id="${i}"><h4>${name[i][1]}</h4></div>`
+    }
+    document.getElementById("chatBody").innerHTML = list
+}
+
+var aayush = JSON.parse(localStorage.getItem('AayushPandey'))
 // console.log(aayush)
 
-// var list = ''
-// console.log(list)
+localStorage.setItem("AayushPandey", JSON.stringify(AayushPandey))
+localStorage.setItem("AryanGupta", JSON.stringify(AryanGupta))
 
-// for (i=0; i < aayush.length; i++)
-// {
-//     console.log(aayush[i])
-//     list += `<div class="chats" id="${i}"><h4>${aayush[i]}</h4></div>`
-// }
 
-var Aayush = [["Aayush Pandey","Hii",1212443],["Avikal Shukla", "Hello", 1223435],["Aayush Pandey", "How are you", 1324355],["Avikal Shukla", "I am good.", 1243545]]
-    
-var Aryan = [["Aryan Gupta","Hey",1212443],["Avikal Shukla", "Hii", 1223435],["Aryan Gupta", "How are you", 1324355],["Avikal Shukla", "I am fine.", 1243545]]
-
-localStorage.setItem("Aayush Pandey", JSON.stringify(Aayush))
-localStorage.setItem("Aryan Gupta", JSON.stringify(Aryan))
-
-// document.getElementById('Aayush Pandey').innerHTML = list
 // console.log(list)
